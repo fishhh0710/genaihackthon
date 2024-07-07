f5b9b1f1a74f4275cb881f0f3461eb1d8052ba84
Gazn000 <106374728+Gazn000@users.noreply.github.com>
2024-07-07 11:10:18 +0800

    Add files via upload

--- src/Personal_team_page.js
+++ src/Personal_team_page.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './Personal_team_page.css';
+import PersonalData from './personalData.json'
+import Edge from './edge.json'
 
 const PersonalTeamPage = () => {
 
   const [isActive, setIsActive] = useState(false); 
+  const [isClick, setIsClick] = useState(false);
+  const [clickedText, setClickedText] = useState('');
+  const [clickedData, setClickedData] = useState(null); // 用于存储点击的 JSON 数据
 
   const handleMenuClick = () => {
     setIsActive(!isActive)
+  };
+
+  const handleClose = () => {
+    setIsClick(false)
+  };
+
+  const handleDetailClick = (event) => {
+    setIsClick(true)
+    const text = event.target.innerText;
+    setClickedText(text);
+
+    const data = PersonalData.find(item => item.data.label === text);
+    setClickedData(data);
   };
 
   return (
@@ -28,19 +46,51 @@ const PersonalTeamPage = () => {
         </div>
       </nav>
       <div className='body'>
-        <div className='Todo'>
+        <section className='Todo'>
             <strong>To-Do</strong>
             <hr />
-            {/* 这里可以添加其他 Team A 相关的内容 */}
-        </div>
-        <div className='inProgress'>
+            <div className='box-container'>
+            {
+        PersonalData && PersonalData.map(personaldata => {
+          return(
+            <div className={`ToDobox${personaldata.data.done}`} key={personaldata.id} onClick={handleDetailClick}>
+              { personaldata.data.label}
+            </div>
+          )
+        })
+      }
+      </div>
+        </section>
+        <section className='inProgress'>
             <strong>In Progress</strong>
             <hr />
-        </div>
-        <div className='Done'>
+            <div className='box-container'>
+            {
+        PersonalData && PersonalData.map(personaldata => {
+          return(
+            <div className={`InProgressbox${personaldata.data.done}`} key={personaldata.id} onClick={handleDetailClick}>
+              { personaldata.data.label}
+            </div>
+          )
+        })
+      }
+      </div>
+        </section>
+        <section className='Done'>
             <strong>Done</strong>
             <hr />
-        </div>
+            <div className='box-container'>
+            {
+        PersonalData && PersonalData.map(personaldata => {
+          return(
+            <div className={`Donebox${personaldata.data.done}`} key={personaldata.id} onClick={handleDetailClick}>
+              { personaldata.data.label}
+            </div>
+          )
+        })
+      }
+      </div>
+        </section>
       </div>
       <div>
       <section className={`menu${isActive ? "isActive" : ""}`}>
@@ -52,7 +102,23 @@ const PersonalTeamPage = () => {
         <hr />
       </section>
     </div>
-
+    <div className={`detail${isClick ? "isClick" : ""}`}>
+    { clickedData && (
+            <div>
+              <h2>{clickedData.data.label}</h2>
+              <i className="material-icons" id='close' onClick={handleClose}>close</i>
+              <hr />
+              <p>Description: 
+                <div className='des_block'>
+                  {clickedData.data.description}
+                </div>
+                </p>
+              <p>Member: {clickedData.data.participants}</p>
+              <p>Deadline: {clickedData.data.deadline}</p>
+              <p>Time: {clickedData.data.time}</p>
+            </div>
+          )}
+    </div>
     </div>
   );
 };
